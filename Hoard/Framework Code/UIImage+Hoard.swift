18661fe137d7775c43b0b7e8aa0b2ded737c2337
Ben Gottlieb <ben@standalone.com>
2015-04-28 14:02:23 -0500

    added data and suggestedFileExtension to UIImage

---
+++ Hoard/Framework Code/UIImage+Hoard.swift
@@ -0,0 +1,19 @@
+//
+//  UIImage+Hoard.swift
+//  Hoard
+//
+//  Created by Ben Gottlieb on 4/28/15.
+//  Copyright (c) 2015 Stand Alone, inc. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+	public var data: NSData {
+		return UIImagePNGRepresentation(self)
+	}
+	
+	public var suggestedFileExtension: String {
+		return "png"
+	}
+}
