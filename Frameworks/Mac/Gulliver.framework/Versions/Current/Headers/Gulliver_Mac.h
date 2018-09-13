7b79adb487751966f5866671f1f84035616812a3
Ben Gottlieb <ben@standalone.com>
2018-09-13 14:55:17 -0500

    updated for Swift 4.2

---
+++ Frameworks/Mac/Gulliver.framework/Versions/Current/Headers/Gulliver_Mac.h
@@ -0,0 +1,19 @@
+//
+//  GulliverMac.h
+//  GulliverMac
+//
+//  Created by Ben Gottlieb on 1/22/15.
+//  Copyright (c) 2015 Stand Alone, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GulliverMac.
+FOUNDATION_EXPORT double GulliverMacVersionNumber;
+
+//! Project version string for GulliverMac.
+FOUNDATION_EXPORT const unsigned char GulliverMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GulliverMac/PublicHeader.h>
+
+
